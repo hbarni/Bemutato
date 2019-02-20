a0fc99246eb1d80f61aee2bad8d2444f7b077c29
hbarni <47736193+hbarni@users.noreply.github.com>
2019-02-20 11:53:29 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-
+                //undefined
+        var tomb = [2,3];
+        res.write(tomb[3]);
+        
 
         res.write("<h1 style='color: blue;'>Teszt OK!</h1>");
         res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
