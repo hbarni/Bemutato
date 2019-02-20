f60f678d5501cfca6d7b2b30fabf64c1fdf321d5
hbarni <47736193+hbarni@users.noreply.github.com>
2019-02-20 11:54:57 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,9 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-                //undefined
-        var tomb = [2,3];
-        res.write(tomb[3]);
+               //undefined
+        //var tomb = [2,3];
+        //res.write(tomb[3]);
+        var a; // szintén undefined típusa lesz
+        res.write(a);
+
+        //null
+        var i = null;
+        res.write(i);
         
 
         res.write("<h1 style='color: blue;'>Teszt OK!</h1>");
